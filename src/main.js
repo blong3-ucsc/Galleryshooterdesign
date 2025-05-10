2f946a5d5fd278f41e91a2bbe25adc3b3223f6c1
blong3-ucsc <blong3@ucsc.edu>
2025-05-09 18:47:48 -0700

    added scenes

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let config = {
     },
     width: 800,
     height: 600,
-    scene: [galleryShooter]
+    scene: [menu, galleryShooter, controls, credits]
 }
 
 const game = new Phaser.Game(config);
